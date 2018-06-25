14f3e617361cc36ac6a7647cec91acc8d7bcf02f
Mark Doeswijk <mark@springtree.nl>
2018-06-25 15:36:34 +0200

    test(demo): Increased buffer and randomized publish times

--- test/client-demo.js
+++ test/client-demo.js
@@ -2,7 +2,7 @@ const NatsBufferedClient = require( '../dist/node-nats-streaming-buffered-client
 
 // Use a buffersize of 10 and a connect timeout of 3 seconds
 //
-const client = new NatsBufferedClient( 10, 3000 );
+const client = new NatsBufferedClient( 3000, 3000 );
 
 // We can publish before connecting
 //
@@ -10,6 +10,7 @@ client.publish( 'test-channel', 'pre-connect' );
 
 // Connect to localhost nats streaming server
 // Can run with: docker run -p 4222:4222 -p 8222:8222 -ti --rm nats-streaming -m 8222 -cid test-cluster
+// Just ctrl+c the docker to test the server being unavailable and start it up again to resume
 //
 const opts = { servers: [ 'nats://localhost:4222' ] };
 client.connect( 'test-cluster', 'demo', opts );
@@ -21,7 +22,7 @@ const interval = setInterval( () =>
   // Publish a message every second
   //
   client.publish( 'test-channel', new Date().toISOString() );
-}, 1000 );
+}, Math.max( 300, (Math.random() * 1000).toFixed(0) ) );
 
 // Stop all the things when we try to quit
 //
